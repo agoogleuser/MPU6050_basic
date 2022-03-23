44cff30428880fcd988983573d193ef3d5f54b47
agoogleuser <armia506@gmail.com>
2022-03-24 00:15:01 +0200

    Completely Rewritten and added readAngle(...)

---
+++ mpu6050_basic.cpp
@@ -0,0 +1,100 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "mpu6050_basic.h"
+
+static float milliOld_mpu = 0;
+static float milliNew_mpu = 0;
+static float dt = 0;
+
+//=======Essential Functions
+void MPU_reg_write(uint8_t regAdr, uint8_t regData)
+{
+    Wire.beginTransmission(MPU_ADDRESS);
+    Wire.write(regAdr);
+    Wire.write(regData);
+    Wire.endTransmission();
+}
+
+uint8_t MPU_reg_read(uint8_t regAdr, int num)
+{ // reads one or two registers (for If it's needed to read a register that is split into 2 banks)
+    uint8_t reading = 0;
+    Wire.beginTransmission(MPU_ADDRESS);
+    Wire.write(regAdr);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_ADDRESS, num, true);
+    if (num == 2)
+        reading = (Wire.read() << 8) | Wire.read();
+    else if (num == 1)
+        reading = Wire.read();
+
+    return reading;
+}
+//=======MPU Initialization
+void mpu_init()
+{
+    Wire.begin();
+    MPU_reg_write(MPU_POWER, 0);
+    MPU_reg_write(GYRO_CONFIG, gyroMode);
+    MPU_reg_write(ACCEL_CONFIG, accelMode);
+}
+//=======Gyroscope Reading
+inline float gyro_read_x()
+{
+    float reading = (float)MPU_reg_read(GYRO_XOUT_H, 2);
+    return reading / gyroConverter;
+}
+inline float gyro_read_y()
+{
+    float reading = (float)MPU_reg_read(GYRO_YOUT_H, 2);
+    return reading / gyroConverter;
+}
+inline float gyro_read_z()
+{
+    float reading = (float)MPU_reg_read(GYRO_ZOUT_H, 2);
+    return reading / gyroConverter;
+}
+// * Added Functions to read the rotation relative to initial starting position
+float readAngle(char axes, bool reset)
+{
+// TODO: Test this compiler error if it works
+#if axes != 'x' || axex != 'y' || axes != 'z'
+#error "Wrong axes input. Correct input should be either 'x', 'y' or 'z'."
+#endif
+    static float yaw = 0;
+    yaw = (reset == true) ? 0 : yaw;
+    milliOld_mpu = milliNew_mpu;
+    milliNew_mpu = millis();
+    dt_mpu = (milliOld_mpu - milliNew_mpu) / 1000;
+    switch (axes)
+    {
+    case 'z': // Yaw
+        float reading = gyro_read_z();
+        break;
+    case 'y': // Pitch
+        float reading = gyro_read_y();
+        break;
+    case 'x': // Roll
+        float reading = gyro_read_x();
+        break;
+    }
+    reading /= converter;
+    yaw += reading * (dt_mpu);
+    return yaw * 2; // For some reason The output here is correct if the case is pm1000.
+}
+
+//=======Accelerometer Reading.
+inline float accel_read_x()
+{
+    float reading = (float)MPU_reg_read(GYRO_XOUT_H, 2);
+    return reading / accelConverter;
+}
+inline float accel_read_y()
+{
+    float reading = (float)MPU_reg_read(GYRO_YOUT_H, 2);
+    return reading / accelConverter;
+}
+inline float accel_read_z()
+{
+    float reading = (float)MPU_reg_read(GYRO_ZOUT_H, 2);
+    return reading / accelConverter;
+}
---
+++ mpu6050_basic.h
@@ -0,0 +1,92 @@
+#ifndef MPU6050_BASIC_H
+#define MPU6050_BASIC_H
+
+//====register addresses
+
+//GYRO and ACCEL control registers
+#define MPU_POWER       0x6B
+#define GYRO_CONFIG     0x1B
+#define ACCEL_CONFIG    0x1A
+
+//three 16-bit registers for reading the accelerometer data
+#define ACCEL_XOUT_H    0x3B
+#define ACCEL_XOUT_L    0x3C
+#define ACCEL_YOUT_H    0x3D
+#define ACCEL_YOUT_L    0X3E
+#define ACCEL_ZOUT_H    0x3F
+#define ACCEL_ZOUT_L    0X40
+
+//three 16-bit registers for reading the accelerometer data
+#define GYRO_XOUT_H     0x43
+#define GYRO_XOUT_L     0x44
+#define GYRO_YOUT_H     0x45
+#define GYRO_YOUT_L     0x46
+#define GYRO_ZOUT_H     0x47
+#define GYRO_ZOUT_L     0x48
+
+//Gyroscope modes
+#define pm250           0b00000
+#define pm500           0b01000
+#define pm1000          0b10000
+#define pm2000          0b11000
+
+
+#ifndef gyroMode
+//Setting a default value for gyroMode.
+    #define gyroMode pm1000
+#endif
+
+#if gyroMode == pm2000
+    #define gyroConverter 131
+#elif gyroMode == pm1000
+    #define gyroConverter 65.5
+#elif gyroMode == pm500
+    #define gyroConverter 32.8
+#else
+    #define gyroConverter 16.4
+#endif
+
+//Accelerometer modes
+#define pm2             0b00000
+#define pm4             0b01000
+#define pm8             0b10000
+#define pm16            0b11000
+
+#ifndef accelMode
+//Setting a default value for gyroMode.
+    #define accelMode pm4
+#endif
+
+#if accelMode == pm16
+    #define accelConverter 2048
+#elif accelMode == pm8
+    #define accelConverter 4096
+#elif accelMode == pm4
+    #define accelConverter 8192
+#else
+    #define accelConverter 16384
+#endif
+
+//====I2C (2-wire) interface
+/*
+ *IMPORTANT NOTE: the I2C must be manually initialized in the arduino code
+ *as a slave device using 'MPU_ADDRESS' as the slave address.
+*/ 
+//max frequency is 400KHz
+#define MPU_ADDRESS     0b1101000 
+#define MPU_SENSOR_ID   0x652
+//needs check: The LSB (A0) of the slave address is variable, can be 0 or 1 
+//depending on the input of AD0 pin.
+
+//====Functions
+void mpu_init();
+
+float gyro_read_x();
+float gyro_read_y();
+float gyro_read_z();
+
+float accel_read_x();
+float accel_read_y();
+float accel_read_z();
+
+#endif
